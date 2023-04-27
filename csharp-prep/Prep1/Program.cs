c651253eaf833dea63608fdbbe322102be5264a4
yillmad <yill.salvatore@gmail.com>
2023-04-27 12:41:58 -0500

    camel case

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,11 +6,11 @@ class Program
     {
         Console.WriteLine("Hello Prep1 World!");
         Console.Write("What is your first name? ");
-        string first_name = Console.ReadLine();
+        string firstName = Console.ReadLine();
 
         Console.Write("What is your last name? ");
-        string last_name = Console.ReadLine();
-        
-        Console.WriteLine($"Your name is {last_name}, {first_name}");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName}");
     }
 }
