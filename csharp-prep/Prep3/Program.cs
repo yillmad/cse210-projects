bbecf437ce04afb8b21627b24b972a69ace6546a
yillmad <yill.salvatore@gmail.com>
2023-04-28 08:00:00 -0500

    Loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string Again = "";
+
+        do
+        {
+            
+            Random randomGenerator = new Random();
+            int IntMagicNumber = randomGenerator.Next(1, 101);
+            Console.WriteLine("Guess the magic number!");
+
+            Console.Write("What is your guess? ");
+            string Guess = Console.ReadLine();
+            int IntGuess = int.Parse(Guess);
+
+            int Attempts = 1;
+
+            while(IntGuess != IntMagicNumber)
+            {
+                if (IntMagicNumber > IntGuess)
+                {
+                Console.WriteLine("Higher");
+                }
+                else if (IntMagicNumber < IntGuess)
+                {
+                Console.WriteLine("Lower");
+                }
+                Console.Write("What is your guess? ");
+                Guess = Console.ReadLine();
+                IntGuess = int.Parse(Guess);
+                Attempts++;
+            }
+
+            Console.WriteLine("You guessed it");
+            Console.WriteLine($"You tried {Attempts} times");
+            Console.Write("Do you want to continue playing? (yes/no)");
+            Again = Console.ReadLine();
+
+        }while (Again == "yes");
+
+        Console.WriteLine("Thanks for playing!");
+
     }
 }
